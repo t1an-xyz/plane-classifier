ec5b9db4784feea226be050fceed7e742a9c464b
Theo <62950302+t1an-xyz@users.noreply.github.com>
2026-08-06 17:16:38 -0700

    Implement app

---
+++ Plane Classifier/Plane Classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plane Classifier
+//
+//  Created by Theo Ma on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plane Classifier/Plane Classifier/Plane_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plane_ClassifierApp.swift
+//  Plane Classifier
+//
+//  Created by Theo Ma on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Plane_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
